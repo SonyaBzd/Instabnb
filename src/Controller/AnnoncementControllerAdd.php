254a1a8249e8a47d823af5dbc53e4d7b8d321dce
Sonya Bouzidi <sonyabouzidi@MacBook-Pro-de-Sonya.local>
2019-09-25 15:33:51 +0200

    Sonya

---
+++ src/Controller/AnnoncementControllerAdd.php
@@ -0,0 +1,38 @@
+<?php
+namespace App\Controller;
+
+use App\DTO\Add;
+use App\Form\AddType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AnnoncementControllerAdd extends AbstractController
+{
+    /**
+     * @Route(
+     *     "/add",
+     *     name="add",
+     *     methods={"GET", "POST"},
+
+     * )
+     */
+    public function index(Request $request)
+
+    {
+        $add = new Add();
+        $form=$this->createForm(AddType::class, $add);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            dump($add);die;
+            return $this->redirectToRoute('home');
+        }
+
+        return $this->render('style/annoncementAdd.html.twig', [
+            'controller_name' => 'AnnoncementControllerAdd',
+            'form'=>$form->createView(),
+        ]);
+    }
+}
---
+++ src/DTO/Contact.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace App\DTO;
+
+
+use Symfony\Component\Validator\Constraints as Assert;
+
+class Contact
+{
+    public $title;
+    public $content;
+    public $price;
+
+}
